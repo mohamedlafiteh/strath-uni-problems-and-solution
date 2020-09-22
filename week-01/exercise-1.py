d535f3b9bf633e030612f3d612c0ba851c7d80c4
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-09-22 18:48:07 +0100

    Add the question to the file

--- week-01/exercise-1.py
+++ week-01/exercise-1.py
@@ -1,3 +1,10 @@
+"""1. Write a function that converts one of {"one", "two", "three", "four", "five",
+"six", "seven", "eight", "nine", "ten"} into a corresponding integer value.
+For example, the function should return 1 when it is given "one".
+The function should have one input argument and one return value. The input
+argument should be a string and the return value should be an integer.
+"""
+
 numbers_list = [
     "one",
     "two",
