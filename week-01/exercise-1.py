f92af415580fe053d04a33b56f43c67a2c290bfd
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-09-22 18:45:21 +0100

    Add user input validation

--- week-01/exercise-1.py
+++ week-01/exercise-1.py
@@ -1,17 +1,26 @@
-
-numbers_list=["one", "two", "three", "four", "five",
-"six", "seven", "eight", "nine", "ten"]
+numbers_list = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+]
 
 
 def converter(user_input):
     if user_input in numbers_list:
         return numbers_list.index(user_input) + 1
-  
-    
 
 
-
-#user_input=input("Please write a number :")
-
-user_input=input("Please inter number : ")
-print(converter(user_input))
+# user_input=input("Please write a number :")
+if __name__ == "__main__":
+    user_input = input("Please inter number : ")
+    if len(user_input) == 0:
+        print("Please enter a valid number")
+    else:
+        print(converter(user_input))
