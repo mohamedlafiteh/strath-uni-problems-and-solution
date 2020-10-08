908791c862d634babf47a8767d79e31f8fe205bb
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-10-08 20:47:41 +0100

    Exercise 1

--- week-03/exercise-1.py
+++ week-03/exercise-1.py
@@ -1,1 +1,29 @@
-print("hello world")
+import math
+
+
+class three_vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def azimuthal_angle(self):
+        if self.x and self.y == 0:
+            return 0
+        else:
+            return math.atan2(self.y, self.x)
+
+    def elevation_angle(self):
+        if self.x and self.y and self.z == 0:
+            return 0
+        else:
+            return math.atan2(math.sqrt((self.x**2) + (self.y**2)), self.z)
+
+    def resultant(self):
+        return math.sqrt(pow(self.x, 2)+pow(self.y, 2)+pow(self.z, 2))
+
+
+test1 = three_vector(10, 20, 30)
+# print(test1.azimuthal_angle())
+# print(test1.elevation_angle())
+print(test1.resultant())
