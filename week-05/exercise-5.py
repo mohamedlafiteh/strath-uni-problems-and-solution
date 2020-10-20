c48bf73bbc96b2640cb018f4c4df9d89a6377fbc
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-10-20 14:27:49 +0100

    week 5

--- week-05/exercise-5.py
+++ week-05/exercise-5.py
@@ -9,23 +9,23 @@ def read_json(file_name):
 
 class Species:
 
-    def __init__(self, code, airQualityIndex):
-        self.code = code
-        self.airQualityIndex = airQualityIndex
+    def __init__(self):
+        self.code = ""
+        self.airQualityIndex = 0
 
     def loadFromJson(self, jsonData):
         self.code = jsonData["code"]
         self.airQualityIndex = jsonData["airQualityIndex"]
 
     def __repr__(self):
-        return f"The code is {self.code}, \n The air quality is {self.airQualityIndex}"
+        return str(f"The code is {self.code}, \n The air quality is {self.airQualityIndex}")
 
 
 class Site:
-    def __init__(self, name, latitude, longitude):
-        self.name = name
-        self.latitude = latitude
-        self.longitude = longitude
+    def __init__(self):
+        self.name = ""
+        self.latitude = 0
+        self.longitude = 0
         self.species = []
 
     def loadFromJson(self, jsonData):
@@ -35,39 +35,39 @@ class Site:
         self.longitude = jsonData["longitude"]
         del self.species[:]
         for jsonSite in jsonData["species"]:
-            species = Species(jsonSite["code"], jsonSite["airQualityIndex"])
+            species = Species()
             species.loadFromJson(jsonSite)
             self.species += [species]
 
+    def average_air_quality(self):
+        print(self.species)
+
 
 def __repr__(self):
-    return f"The name is {self.name}, \n The latitude is {self.latitude} \n  The longitude is {self.longitude}. \n The Species are {self.species}"
+    return str(f"The name is {self.name}, \n The latitude is {self.latitude} \n  The longitude is {self.longitude}. \n The Species are {str(self.species)}")
 
 
 class LocalAuthority:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.name = ""
         self.sites = []
 
     def loadFromJson(self, jsonData):
         self.name = jsonData["name"]
         del self.sites[:]
         for jsonSite in jsonData["sites"]:
-            site = Site(jsonSite["name"],
-                        jsonSite["latitude"], jsonSite["longitude"])
+            site = Site()
             site.loadFromJson(jsonSite)
             self.sites += [site]
 
     def __repr__(self):
-        return f"The name is {self.name}, \n The site list is {self.sites}"
+        return f"The name is {self.name}, \n The site list is {str(self.sites)}"
 
 
 if __name__ == "__main__":
     js_data = read_json("./AirQuality.json")
-    data_m = {}
+
     for data in js_data:
-        data_m.update(data)
-
-
-authority = LocalAuthority(data_m["name"])
-authority.loadFromJson(data_m)
+        authority = LocalAuthority()
+        authority.loadFromJson(data)
+        print(authority.sites["name"])
